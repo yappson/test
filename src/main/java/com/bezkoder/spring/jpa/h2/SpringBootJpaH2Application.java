2594fa859eb3a3129f7629072d1c51b022917dc9
yatendra.singh <11352997+yappson@users.noreply.github.com>
2023-11-27 17:18:25 +0530

    webhook test4

--- src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class SpringBootJpaH2Application extends SpringBootServletInitializer {
-// hello 123 pushs testkasjdnkjsandkjas
+// hello 123 pushs testkasjdnkjsandkjaskjsandkjasnkjdsnb
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpringBootJpaH2Application.class);
