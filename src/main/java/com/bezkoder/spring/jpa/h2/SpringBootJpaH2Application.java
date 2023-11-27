4914149ae46fc70644f788aeb6ccb69f5aec8f04
yatendra.singh <11352997+yappson@users.noreply.github.com>
2023-11-27 17:58:37 +0530

    final Tets

--- src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class SpringBootJpaH2Application extends SpringBootServletInitializer {
-// hello 123 pushs testkasjdnkjsandkjaskjsandkjasnkjdsnb
+// hello 123 pushs testkasjdnkjsandkjaskjsandkjasnkjdsnbhsagvdfgjasvdjhgdfvjhasdgv
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpringBootJpaH2Application.class);
