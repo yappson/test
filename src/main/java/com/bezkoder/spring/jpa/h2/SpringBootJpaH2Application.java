a5306f4f9498a1e9652623976fd11b509112f730
yatendra.singh <11352997+yappson@users.noreply.github.com>
2023-12-15 10:54:13 +0530

    test14

--- src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -6,12 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootJpaH2Application extends SpringBootServletInitializer {
-// hello 123 pushs testkasjdnkjsandkjaskjsandkjasnkjdsnbhsagvdfgjasvdjhgdfvjhasdgvdsjbjkdsabkjbaskjb
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringBootJpaH2Application.class);
-	}
+public class SpringBootJpaH2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaH2Application.class, args);
--- src/main/java/com/bezkoder/spring/jpa/h2/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/repository/TutorialRepository.java
@@ -8,6 +8,6 @@ import com.bezkoder.spring.jpa.h2.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
   List<Tutorial> findByPublished(boolean published);
-
+  // finby
   List<Tutorial> findByTitleContainingIgnoreCase(String title);
 }
